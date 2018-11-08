59dc53897d2dcf2aec0e2419899bed6a06c8a6df
fede4ka <40442383+fede4ka@users.noreply.github.com>
2018-11-09 02:41:03 +0300

    Delete question6.scala

--- src/main/scala/Ultraeasyquestions/question6.scala
+++
@@ -1,8 +0,0 @@
-package UltraEasyQuestions
-
-//print the 12x12 multiplication table
-
-object question6 extends App{
-  val a = for {x <- 1 to 12} yield List.range(1,13).map(n=>n * x)
-  a foreach(println)
-}
